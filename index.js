6bcd19979eb356f4c1613d2db78c42bd48191d99
mrkcancode <motasimkazmi333@gmail.com>
2020-06-07 10:04:40 +0530

    setup static files and partial and layouts

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('assets'));
+app.use(expressLayouts);
+
+//Extract the stles and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //Use express router
 app.use('/', require('./routes'));
