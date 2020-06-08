7768815ee6c5087d2704a3ab28d99cd4e1865416
mrkcancode <motasimkazmi333@gmail.com>
2020-06-08 23:22:13 +0530

    signup done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,58 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
         title: 'User Profile'
     });
 }
+
+
+
+//Render the signup page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: 'Codeial | SignUp'
+    });
+}
+
+
+//Render the sign In page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: 'Codeial | SignIn'
+    });
+};
+
+module.exports.create = function(req, res){
+    if(req.body.password!= req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log("Error in ifnding user in signing up"); return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log("Error in creating user while singing up");
+                
+               return
+            }
+
+            return res.redirect('/users/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+//signin and create a session for the user
+module.exports.createSession = function(req, res){
+    //TODO later
+}
+
+
+//after clicking on signup it should direct to sign in rght
+//yes
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 const usersContoller = require('../controllers/users_controller');
-const postsController = require('../controllers/posts_controller');
+// const postsController = require('../controllers/posts_controller');
 
 router.get('/profile', usersContoller.profile);
 
-router.get('/posts', postsController.posts);
+// router.get('/posts', postsController.posts);
+
+router.get('/sign-up', usersContoller.signUp);
+router.get('/sign-in', usersContoller.signIn);
+
+router.post('/create', usersContoller.create);
 module.exports = router;
+
+
+//this sigin router is just for rendering the basic sigin page..
+//i havent created the createSession thing yet
+//u need to do that
+//but in the lecture video it was working wihtout doing that
+//let me see
