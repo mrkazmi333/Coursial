00ec2d86b9c77e53f4d9e4946083130daaffe375
mrkcancode <motasimkazmi333@gmail.com>
2020-06-17 21:20:48 +0530

    display posts and related user

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,7 +1,21 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-        return res.render('home',{
-        title: 'home'
-    });
+
+        // Post.find({}, function(err, posts){
+        //     return res.render('home',{
+        //         title: 'Codeial | Home',
+        //         posts: posts
+        //     });
+        // });
+
+        //Populate the user for each post
+        Post.find({}).populate('user').exec(function(err, posts){
+            return res.render('home',{
+                title: 'Codeial | Home',
+                posts: posts
+            });
+        });
 }
