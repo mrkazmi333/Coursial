5ba9b404b34431b00aa9cb1287e28c72d003668f
mrkcancode <motasimkazmi333@gmail.com>
2020-06-17 12:55:39 +0530

    save posts to db

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,12 @@
-module.exports.posts = function(rq, res){
-    return res.end('<h1>This is a post</h1>');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+
+    }, function(err, post){
+        if(err){console.log('Error in creating post'); return;}
+        return res.redirect('back');
+    });
 }
